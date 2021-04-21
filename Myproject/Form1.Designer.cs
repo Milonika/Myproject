31413bd7f6700af27785f9353dc7d31f4cab6a73
алина алина <fkbyf-2003zv@mail.ru>
2021-04-21 14:52:46 +0300

    add form2

--- Myproject/Form1.Designer.cs
+++ Myproject/Form1.Designer.cs
@@ -30,68 +30,76 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.Reactangle = new System.Windows.Forms.Button();
-            this.Circle = new System.Windows.Forms.Button();
-            this.Triangle = new System.Windows.Forms.Button();
             this.Line = new System.Windows.Forms.Button();
+            this.Triangle = new System.Windows.Forms.Button();
+            this.Circle = new System.Windows.Forms.Button();
+            this.Reactangle = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(99, 12);
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel1.Location = new System.Drawing.Point(112, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(689, 426);
+            this.panel1.Size = new System.Drawing.Size(676, 426);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel2.Controls.Add(this.Line);
             this.panel2.Controls.Add(this.Triangle);
             this.panel2.Controls.Add(this.Circle);
             this.panel2.Controls.Add(this.Reactangle);
             this.panel2.Location = new System.Drawing.Point(12, 9);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(81, 426);
+            this.panel2.Size = new System.Drawing.Size(94, 426);
             this.panel2.TabIndex = 1;
             // 
-            // Reactangle
+            // Line
             // 
-            this.Reactangle.Location = new System.Drawing.Point(13, 29);
-            this.Reactangle.Name = "Reactangle";
-            this.Reactangle.Size = new System.Drawing.Size(47, 43);
-            this.Reactangle.TabIndex = 0;
-            this.Reactangle.UseVisualStyleBackColor = true;
-            this.Reactangle.Click += new System.EventHandler(this.Reactangle_Click);
-            // 
-            // Circle
-            // 
-            this.Circle.Location = new System.Drawing.Point(13, 96);
-            this.Circle.Name = "Circle";
-            this.Circle.Size = new System.Drawing.Size(47, 42);
-            this.Circle.TabIndex = 1;
-            this.Circle.UseVisualStyleBackColor = true;
-            this.Circle.Click += new System.EventHandler(this.Circle_Click);
+            this.Line.Font = new System.Drawing.Font("Monotype Corsiva", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Line.Location = new System.Drawing.Point(13, 270);
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(65, 57);
+            this.Line.TabIndex = 3;
+            this.Line.Text = "Line";
+            this.Line.UseVisualStyleBackColor = true;
+            this.Line.Click += new System.EventHandler(this.Line_Click);
             // 
             // Triangle
             // 
-            this.Triangle.Location = new System.Drawing.Point(13, 157);
+            this.Triangle.Image = global::Myproject.Properties.Resources.df;
+            this.Triangle.Location = new System.Drawing.Point(13, 196);
             this.Triangle.Name = "Triangle";
-            this.Triangle.Size = new System.Drawing.Size(47, 41);
+            this.Triangle.Size = new System.Drawing.Size(65, 68);
             this.Triangle.TabIndex = 2;
             this.Triangle.UseVisualStyleBackColor = true;
             this.Triangle.Click += new System.EventHandler(this.Triangle_Click);
             // 
-            // Line
+            // Circle
             // 
-            this.Line.Location = new System.Drawing.Point(13, 220);
-            this.Line.Name = "Line";
-            this.Line.Size = new System.Drawing.Size(47, 44);
-            this.Line.TabIndex = 3;
-            this.Line.UseVisualStyleBackColor = true;
-            this.Line.Click += new System.EventHandler(this.Line_Click);
+            this.Circle.Image = global::Myproject.Properties.Resources._205_2051215_bluecircle_circle_blue_trend_glowblue_glow_trends_blue;
+            this.Circle.Location = new System.Drawing.Point(13, 123);
+            this.Circle.Name = "Circle";
+            this.Circle.Size = new System.Drawing.Size(65, 67);
+            this.Circle.TabIndex = 1;
+            this.Circle.UseVisualStyleBackColor = true;
+            this.Circle.Click += new System.EventHandler(this.Circle_Click);
+            // 
+            // Reactangle
+            // 
+            this.Reactangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Reactangle.Image = global::Myproject.Properties.Resources._9369366693_stickermin_yapfiles_ru;
+            this.Reactangle.Location = new System.Drawing.Point(13, 56);
+            this.Reactangle.Name = "Reactangle";
+            this.Reactangle.Size = new System.Drawing.Size(65, 61);
+            this.Reactangle.TabIndex = 0;
+            this.Reactangle.UseVisualStyleBackColor = true;
+            this.Reactangle.Click += new System.EventHandler(this.Reactangle_Click);
             // 
             // Form1
             // 
---
+++ Myproject/Form2.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Myproject
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.width = new System.Windows.Forms.Label();
+            this.heigth = new System.Windows.Forms.Label();
+            this.color = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(125, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // width
+            // 
+            this.width.AutoSize = true;
+            this.width.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.width.Location = new System.Drawing.Point(46, 69);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(48, 17);
+            this.width.TabIndex = 3;
+            this.width.Text = "Width:";
+            // 
+            // heigth
+            // 
+            this.heigth.AutoSize = true;
+            this.heigth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.heigth.Location = new System.Drawing.Point(46, 109);
+            this.heigth.Name = "heigth";
+            this.heigth.Size = new System.Drawing.Size(53, 17);
+            this.heigth.TabIndex = 4;
+            this.heigth.Text = "Heigth:";
+            // 
+            // color
+            // 
+            this.color.AutoSize = true;
+            this.color.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.color.Location = new System.Drawing.Point(46, 152);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(45, 17);
+            this.color.TabIndex = 5;
+            this.color.Text = "Color:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Введите следующий значения:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 226);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.color);
+            this.Controls.Add(this.heigth);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label width;
+        private System.Windows.Forms.Label heigth;
+        private System.Windows.Forms.Label color;
+        private System.Windows.Forms.Label label4;
+    }
+}
